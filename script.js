29912ab3ce9fc4e5fceac9ceb7d53b3b5a1d36df
Crolloh <latrellbang@gmail.com>
2026-01-05 21:06:01 +0800

    added transition logic

---
+++ script.js
@@ -0,0 +1,16 @@
+function beforeAfter(to, from) {
+    document.getElementById(from).classList.remove('active')
+    document.getElementById(to).classList.add('active')
+}
+
+function toMenu(event) {
+    let name = document.getElementById('name').value
+    if (!name) return alert('Please Enter your name!')
+    event.preventDefault();
+    beforeAfter('menuscreen', 'namescreen')
+
+}
+
+function startGame() {
+    beforeAfter('gamescreen', 'menuscreen')
+}
